c3ff75fcd095e81969843d9a139ac8d35ef2f14a
Johan Dewe <johan@dewe.net>
2014-01-02 23:21:11 +0100

    ex1

---
+++ PyLab/ex1.py
@@ -0,0 +1,10 @@
+# Printing statement is replaced with function in python 3.0
+# http://docs.python.org/3.0/whatsnew/3.0.html#print-is-a-function
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print ('I "said" do not touch this.')
